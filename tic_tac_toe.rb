823da32a918a712aee412e87133ed0cef3af87ec
vepardoa <violetaepardo@gmail.com>
2023-03-22 18:00:29 -0600

    Add validation helper file and methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
 
-# Top level documentation for class Game
-class Game
-  def initialize
-    @board = Array.new(9, '-')
-  end
+board = Array.new(9, ' ')
 
-  def print_board
-    p "#{@board[0]}|#{@board[1]}|#{@board[2]}"
-    p "#{@board[3]}|#{@board[4]}|#{@board[5]}"
-    p "#{@board[6]}|#{@board[7]}|#{@board[8]}"
-  end
+def print_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts '---+---+---'
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts '---+---+---'
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
 
-game = Game.new
-game.print_board
+def player_selection(position_selected)
+  position_selected.to_i - 1
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def position_taken?(board, index)
+  return false if (board[index] == ' ') || (board[index] == '') || board[index].nil?
+
+  true
+end
+
+print_board(board)
